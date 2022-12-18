bc53a92e09da87982aa38fd9052174b938faa673
TinuBose <117371766+TinuBose@users.noreply.github.com>
2022-12-18 20:17:59 +0530

    Update circular_queue.c

--- circular_queue.c
+++ circular_queue.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int queue[50], x, n,choice;
+int queue[50], x, n, choice;
 int front = -1;
 int rear = -1;
 void enqueue();
@@ -36,49 +36,49 @@ void main() {
   while (choice != 4);
 }
 void enqueue() {
-    if ((rear+1)%n == front) {
-      printf("\noverflow\n");
-   
-    }else{
-      printf("\nenter element to insert\n");
-      scanf("%d",&x);
-    if(front==-1 && rear==-1)  {
-         front = rear = 0;
-        queue[rear] = x;
-    }else{
-	rear=(rear+1)%n;
-        queue[rear]=x;
-  }
-	printf("\nenqued=%d",queue[rear]);
-}
-}
+  if ((rear + 1) % n == front) {
+    printf("\noverflow\n");
 
+  } else {
+    printf("\nenter element to insert\n");
+    scanf("%d", & x);
+    if (front == -1 && rear == -1) {
+      front = rear = 0;
+      queue[rear] = x;
+    } else {
+      rear = (rear + 1) % n;
+      queue[rear] = x;
+    }
+    printf("\nenqued=%d", queue[rear]);
+  }
+}
 
 void dequeue() {
-	if(front == -1 && rear == -1){
-	printf("\nunderflow\n");
-	
-       }else{
-	printf("\ndequed=%d\n",queue[front]);
-	if(front == rear){
-         front = rear = -1;
-	}else{
-	front=(front+1)%n;
-	
-	
-}
+  if (front == -1 && rear == -1) {
+    printf("\nunderflow\n");
 
-}
+  } else {
+    printf("\ndequed=%d\n", queue[front]);
+    if (front == rear) {
+      front = rear = -1;
+    } else {
+      front = (front + 1) % n;
+
+    }
+
+  }
 }
 
 void display() {
-	int i;
-       if(front == -1){
-	printf("\nunderflow\n");
-}else{
-	for(i=front;i<=rear;i++){
-	printf("%d ",queue[i]);
-}
-}
+  int i = front;
+  if (front == -1 && rear == -1) {
+    printf("\nunderflow\n");
+  } else {
+    while (i != rear) {
+      printf("%d ", queue[i]);
+      i = (i + 1) % n;
+    }
+    printf("%d", queue[rear]);
+  }
 
 }
